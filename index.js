a3bf1f5a7cf2f26a8f548b65886db04deadd4bab
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-20 03:51:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
   console.log('Happy birthday, Cody!')
 }
